8e912c045661b48ba5ebc7a1836f7bf759cb5c39
Peter Viszt <gtpassatgt@gmail.com>
2021-05-23 15:24:25 +0200

    github link

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Footer = () => {
 	return (
 		<footer>
-			<p>Ez a weboldal nem tárol és kezel adatot, de azért itt van a kötelező <Link href="/adatvedelmi-nyilatkozat"><a>adatvédelmi nyilatkozat</a></Link>. A forráskód elérhető a <a href="#">Github</a>-on. Ha tetszik, nézd meg a <a href="https://itunes.apple.com/hu/app/heart-reports/id1448243870" target="_blank">❤️ Heart Reports</a> appomat is. Kapcsolatba léphetsz velem az <a href="mailto:info@visztpeter.me">info@visztpeter.me</a> címen.</p>
+			<p>Ez a weboldal nem tárol és kezel adatot, de azért itt van a kötelező <Link href="/adatvedelmi-nyilatkozat"><a>adatvédelmi nyilatkozat</a></Link>. A forráskód elérhető a <a href="https://github.com/passatgt/vakcinapass" target="_blank">Github</a>-on. Ha tetszik, nézd meg a <a href="https://itunes.apple.com/hu/app/heart-reports/id1448243870" target="_blank">❤️ Heart Reports</a> appomat is. Kapcsolatba léphetsz velem az <a href="mailto:info@visztpeter.me">info@visztpeter.me</a> címen.</p>
 		</footer>
 	);
 };
