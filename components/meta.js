02b86dd7835fdf89b09dbb75efe2d95461f7170c
Peter Viszt <gtpassatgt@gmail.com>
2021-05-23 13:37:50 +0200

    Favicon fix

--- components/meta.js
+++ components/meta.js
@@ -15,14 +15,14 @@ const Meta = () => {
 			<meta property="twitter:description" content="Add hozzá a védettési igazolványod az Apple Wallet-hez." />
 			<meta property="twitter:image" content="https://vakcina-pass.herokuapp.com/static/images/og-meta.png" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<link rel="apple-touch-icon" sizes="180x180" href="/static/imagesapple-touch-icon-180x180-precomposed.png" />
-	    <link rel="apple-touch-icon" sizes="152x152" href="/static/imagesapple-touch-icon-152x152-precomposed.png" />
-	    <link rel="apple-touch-icon" sizes="144x144" href="/static/imagesapple-touch-icon-144x144-precomposed.png" />
-	    <link rel="apple-touch-icon" sizes="114x114" href="/static/imagesapple-touch-icon-114x114-precomposed.png" />
-	    <link rel="apple-touch-icon" sizes="72x72" href="/static/imagesapple-touch-icon-72x72-precomposed.png" />
-	    <link rel="apple-touch-icon" href="/static/imagesapple-touch-icon-precomposed.png" />
-	    <link rel="shortcut icon" href="/static/imagesfavicon.png" />
-	    <link rel="icon" type="image/ico" href="/static/imagesfavicon.ico" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon-180x180-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="152x152" href="/static/images/apple-touch-icon-152x152-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="144x144" href="/static/images/apple-touch-icon-144x144-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="114x114" href="/static/images/apple-touch-icon-114x114-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="72x72" href="/static/images/apple-touch-icon-72x72-precomposed.png" />
+	    <link rel="apple-touch-icon" href="/static/images/apple-touch-icon-precomposed.png" />
+	    <link rel="shortcut icon" href="/static/images/favicon.png" />
+	    <link rel="icon" type="image/ico" href="/static/images/favicon.ico" />
 		</>
 	);
 };
