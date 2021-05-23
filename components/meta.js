9fb660748c2b676a4ba739826a1034bedadca1a2
Peter Viszt <gtpassatgt@gmail.com>
2021-05-23 13:17:31 +0200

    Favicons

--- components/meta.js
+++ components/meta.js
@@ -6,19 +6,23 @@ const Meta = () => {
 			<meta name="description" content="Add hozzá a védettési igazolványod az Apple Wallet-hez." />
 			<meta property="og:description" content="Add hozzá a védettési igazolványod az Apple Wallet-hez." />
 			<meta property="og:site_name" content="Védettségi igazolvány Apple Wallet-hez" />
-			<meta property="og:image" content="https://vakcina-pass.herokuapp.com/static/images/logo.png" />
+			<meta property="og:image" content="https://vakcina-pass.herokuapp.com/static/images/og-meta.png" />
 			<meta property="og:image:alt" content="WiFi Pass"/>
 			<meta property="og:image:width" content="1200"/>
-			<meta property="og:image:height" content="1200"/>
+			<meta property="og:image:height" content="630"/>
 			<meta name="twitter:card" content="summary"/>
 			<meta name="twitter:creator" content="@visztpeter" />
 			<meta property="twitter:description" content="Add hozzá a védettési igazolványod az Apple Wallet-hez." />
-			<meta property="twitter:image" content="https://vakcina-pass.herokuapp.com/static/images/logo.png" />
+			<meta property="twitter:image" content="https://vakcina-pass.herokuapp.com/static/images/og-meta.png" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicons/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="192x192" href="/static/images/favicons/favicon-192x192.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicons/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicons/favicon-16x16.png" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/static/imagesapple-touch-icon-180x180-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="152x152" href="/static/imagesapple-touch-icon-152x152-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="144x144" href="/static/imagesapple-touch-icon-144x144-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="114x114" href="/static/imagesapple-touch-icon-114x114-precomposed.png" />
+	    <link rel="apple-touch-icon" sizes="72x72" href="/static/imagesapple-touch-icon-72x72-precomposed.png" />
+	    <link rel="apple-touch-icon" href="/static/imagesapple-touch-icon-precomposed.png" />
+	    <link rel="shortcut icon" href="/static/imagesfavicon.png" />
+	    <link rel="icon" type="image/ico" href="/static/imagesfavicon.ico" />
 		</>
 	);
 };
